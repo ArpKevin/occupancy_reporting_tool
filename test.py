f20ab747f569981dc256d0cfabc97f4ada6df14b
Kevin <arpaskevin11@gmail.com>
2026-03-03 12:36:01 +0100

    refactor: move os.path.isfile check to pure, testable function

--- test.py
+++ test.py
@@ -8,11 +8,14 @@ def normalize_file_name(file_name, file_extension):
         return file_name + file_extension
     return file_name
 
+def is_existing_file(file_name):
+    return os.path.isfile(file_name)
+
 def get_file_name():
     while True:
         file_name = input("Enter the name of the xlsx file: ")
         file_name = normalize_file_name(file_name, ".xlsx")
-        if os.path.isfile(file_name):
+        if is_existing_file(file_name):
             return file_name
         else:
             print("Invalid file name. Try again.")
